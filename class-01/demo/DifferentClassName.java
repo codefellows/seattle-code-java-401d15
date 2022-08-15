1b6930712a2deac9e8852c2f19faee0c7d980b4b
Instructor Alex White <alex.white@codefellows.com>
2022-08-15 13:21:47 -0700

    demo code

---
+++ class-01/demo/DifferentClassName.java
@@ -0,0 +1,10 @@
+public class DifferentClassName{
+  // parameter!!! or argument? 
+  // parameters are for method definitions
+  // arguments are actual value.
+  public static void main(String[] args){
+    // console.log
+    //SOUT
+    System.out.println("This is working!!!");
+  }
+}
