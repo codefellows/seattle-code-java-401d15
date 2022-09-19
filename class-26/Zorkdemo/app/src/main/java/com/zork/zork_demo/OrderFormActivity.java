f30e12b4566639a244a66e332dfa1a33cf81dcb4
Instructor Alex White <alex.white@codefellows.com>
2022-09-19 14:58:43 -0700

    class 26 demo

---
+++ class-26/Zorkdemo/app/src/main/java/com/zork/zork_demo/OrderFormActivity.java
@@ -0,0 +1,14 @@
+package com.zork.zork_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_form);
+    }
+}
